2698d4b21bd251b0674cd1e7d964fce929b8ccc4
Marcelo Alves <askbl130@bancoctt.pt>
2021-03-07 23:00:09 +0000

    first tests

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,75 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
+public class TaskControllerTest {
+
+    @Mock
+    private TaskRepo taskRepo;
+
+    @InjectMocks
+    private TaskController taskController;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemDescricao() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+        try {
+            taskController.save(todo);
+            Assert.fail();
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the task description", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemData() {
+        Task todo = new Task();
+        todo.setTask("Descricao");
+        try {
+            taskController.save(todo);
+            Assert.fail();
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the due date", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaComDataPassada() {
+        Task todo = new Task();
+        todo.setTask("Descricao");
+        todo.setDueDate(LocalDate.of(2010,01,01));
+        try {
+            taskController.save(todo);
+            Assert.fail();
+        } catch (ValidationException e) {
+            Assert.assertEquals("Due date must not be in past", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveSalvarTarefaCompleta() throws ValidationException {
+        Task todo = new Task();
+        todo.setTask("Descricao");
+        todo.setDueDate(LocalDate.now());
+        taskController.save(todo);
+
+        Mockito.verify(taskRepo).save(todo);
+    }
+}
